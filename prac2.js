290643385483bf019bf9e65f5b348963e171f65f
blvck <omarfaraj355@gmail.com>
2024-02-26 22:59:23 +0300

    javascript-fundamentals

---
+++ prac2.js
@@ -0,0 +1,19 @@
+//common string methods
+
+let email = 'omarfarajfun123@gmail.com';
+
+//let result = email1.LastIndexOf('a');
+//console.log(email1);
+
+//var email2 = 'omarfarajfun123@gmial.com'
+
+//let result = email2.slice(0,11)
+//console.log(emial2)
+
+//let email3 = 'omarfarajfun123@gmial.com'
+
+//let result = email.substr(5,10)
+
+let result = email.replace('f', 'j');
+
+console.log(result);
